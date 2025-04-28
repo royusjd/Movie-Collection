ba2a3e1d8690bba430c73fc8d1b16ee96ef56e82
royusjd <juodeikisrojus2005@gmail.com>
2025-04-28 11:05:01 +0300

    Added get and post methods

--- movie-backend/index.js
+++ movie-backend/index.js
@@ -11,10 +11,52 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 app.get("/", (req, res) => {
 	res.send("Movie Collection API is working!");
 });
 
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+app.get("/movies", (req, res) => {
+	database.query("SELECT * FROM movie", (err, results) => {
+		if (err) {
+			console.error("Failed to get movies:", err);
+			res.status(500).json({ error: "Database error" });
+		} else {
+			res.json(results);
+		}
+	});
+});
+
+app.post("/movies", (req, res) => {
+	const { title, description, release_year, rating, director_id, image_url } =
+		req.body;
+
+	const sql = `
+		INSERT INTO movie (title, description, release_year, rating, director_id, image_url)
+		VALUES (?, ?, ?, ?, ?, ?)
+	`;
+
+	const values = [
+		title,
+		description,
+		release_year,
+		rating,
+		director_id,
+		image_url,
+	];
+
+	database.query(sql, values, (err, result) => {
+		if (err) {
+			console.error("Failed to add movie:", err);
+			res.status(500).json({ error: "Database error" });
+		} else {
+			res.status(201).json({
+				message: "Movie added successfully!",
+				movieId: result.insertId,
+			});
+		}
+	});
 });
