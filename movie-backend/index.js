26c63a0695735282cd0d5011f0d643ff2f709755
royusjd <juodeikisrojus2005@gmail.com>
2025-04-28 15:09:07 +0300

    Added register post

--- movie-backend/index.js
+++ movie-backend/index.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { body, validationResult } from "express-validator";
 import database from "./database.js";
 
 dotenv.config();
@@ -104,3 +107,45 @@ app.delete("/movies/:id", (req, res) => {
 		res.status(200).json({ message: "Movie deleted successfully!" });
 	});
 });
+
+app.post(
+	"/register",
+	[
+		body("email").isEmail().withMessage("Please enter a valid email."),
+		body("password")
+			.isLength({ min: 6 })
+			.withMessage("Password should be at least 6 characters."),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { username, email, password } = req.body;
+
+		try {
+			const [userExists] = await database
+				.promise()
+				.query("SELECT * FROM users WHERE email = ?", [email]);
+
+			if (userExists.length > 0) {
+				return res.status(400).json({ message: "User already exists." });
+			}
+
+			const hashedPassword = await bcrypt.hash(password, 10);
+
+			const result = await database
+				.promise()
+				.query(
+					"INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+					[username, email, hashedPassword]
+				);
+
+			res.status(201).json({ message: "User registered successfully." });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: "Server error" });
+		}
+	}
+);
